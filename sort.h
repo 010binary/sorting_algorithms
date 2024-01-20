86412cfba26fa4fb40233eac4e2bb428e0bd9cfa
james-eo <jameseokonkwo@gmail.com>
2024-01-20 13:56:54 +0100

    Sort array with bubble sort

--- sort.h
+++ sort.h
@@ -36,6 +36,7 @@ typedef struct listint_s
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 
 
 #endif
