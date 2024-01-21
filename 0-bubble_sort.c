d505450f0625d8accc7ccbba3100e26e94a56238
010binary <augustine32fg@gmail.com>
2024-01-21 02:53:30 -0800

    trying to adjust the bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include "sort.h"
+
 /**
  * bubble_sort - Sorts an array of integers
- * in ascending order using Bubble Sort
+ * in ascend order using Bubble Sort
  * @array: The array to be sorted
- * @size: Number of elements in the array
+ * @size: The size of the array
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
@@ -19,14 +20,20 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
+				/* Swap the elements */
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
+				/* Set swapped flag to true */
 				swapped = 1;
+
+				/* Print the array after each swap */
 				print_array(array, size);
 			}
 		}
 
+		/* if no two elements were swapped by inner loop, array is already sorted */
 		if (swapped == 0)
 			break;
 	}
