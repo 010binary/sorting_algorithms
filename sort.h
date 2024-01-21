a8cccb1dd6d12b430ca31ee0bed205e92481bfc7
010binary <augustine32fg@gmail.com>
2024-01-21 02:49:18 -0800

    3-quick_sort

--- sort.h
+++ sort.h
@@ -40,6 +40,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void lomuto_qsort(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif
